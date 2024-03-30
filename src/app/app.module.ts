167e150ab38442f240c3a00c6c8138d3f462a110
Harald Nezbeda <hn@nezhar.com>
2024-03-30 09:21:19 +0100

    Upgrade ionic and angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
